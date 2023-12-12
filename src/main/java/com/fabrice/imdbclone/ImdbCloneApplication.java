c7d1f16b2719c5bdff4172e0705a186f0a0f5afa
Fabrice Niyongabo <ninga.fab@gmail.com>
2023-12-12 20:02:07 +0200

    init

---
+++ src/main/java/com/fabrice/imdbclone/ImdbCloneApplication.java
@@ -0,0 +1,13 @@
+package com.fabrice.imdbclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImdbCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImdbCloneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fabrice/imdbclone/ImdbCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fabrice.imdbclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImdbCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
