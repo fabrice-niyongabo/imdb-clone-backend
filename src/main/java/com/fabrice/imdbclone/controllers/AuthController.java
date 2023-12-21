2be42e64842a5e7e695f66fbb8362e8a6e3084f9
Fabrice Niyongabo <ninga.fab@gmail.com>
2023-12-21 23:04:47 +0200

    adding cors

--- src/main/java/com/fabrice/imdbclone/controllers/AuthController.java
+++ src/main/java/com/fabrice/imdbclone/controllers/AuthController.java
@@ -8,14 +8,14 @@ import com.fabrice.imdbclone.dto.SignupRequest;
 import com.fabrice.imdbclone.models.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
+//@CrossOrigin //by default it allows all origins
+@CrossOrigin(origins = "*")
+
 public class AuthController {
     private  final AuthenticationService authenticationService;
 
