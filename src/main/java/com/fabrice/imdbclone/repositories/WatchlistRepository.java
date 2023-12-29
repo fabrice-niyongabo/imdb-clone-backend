b63aa6886ef86f62eaed6cd5a8fb991d1db8240f
Fabrice Niyongabo <ninga.fab@gmail.com>
2023-12-29 18:26:27 +0200

    ft: deletin from watchlist by user id and movie id

--- src/main/java/com/fabrice/imdbclone/repositories/WatchlistRepository.java
+++ src/main/java/com/fabrice/imdbclone/repositories/WatchlistRepository.java
@@ -2,9 +2,11 @@ package com.fabrice.imdbclone.repositories;
 
 import com.fabrice.imdbclone.models.Watchlist;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.Optional;
 
 
 public interface WatchlistRepository extends JpaRepository<Watchlist, Long> {
@@ -12,5 +14,17 @@ public interface WatchlistRepository extends JpaRepository<Watchlist, Long> {
 
     boolean existsByUserIdAndMovieId(Long userId, Long movieId);
 
+    //deleting by adding custom query
+    //@Transactional
+    //@Modifying
+    //@Query("DELETE FROM Watchlist WHERE userId= :userId AND movieId= :movieId")
+    //void deleteByUserIdAndMovieId(Long userId, Long movieId);
+
+
+    //@Transactional annotation ensures the operation is wrapped within a transaction
+    //and is required by @Modifying
+    @Transactional
+    //specifying that this is an update or delete command
+    @Modifying
     void deleteByUserIdAndMovieId(Long userId, Long movieId);
 }
